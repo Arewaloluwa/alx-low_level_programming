d4ebdea479255b6c2f5e31365c5ff9e587bb6903
Arewaloluwa <arewaakinbayo@gmail.com>
2022-09-16 21:39:03 -0700

    pp

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - create an infinte loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinte loop incoming :(\n");
+	
+	i = 0;
+
+	/*while (i<10) */
+	/*{ */
+	/*	putchar(i);*/
+	/*} */
+
+	printf("indefinite loop avoided! \\o/\n");
+
+	return (0);
+}
