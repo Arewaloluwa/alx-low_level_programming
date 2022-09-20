6420fd631cf77edeae26a509576e80c34b2f8fe6
Arewaloluwa <arewaakinbayo@gmail.com>
2022-09-20 16:46:59 -0700

    pop

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 /**
  * _strlen - gets character string
  *
- * s: character to get string
+ * @s: character to get string
  *
  * Return - SUCCESS
  *
@@ -10,7 +10,6 @@
 int _strlen(char *s)
 {
 	int i;
-	
 	for (i = 0; s[i] != '\0'; i++)
 	{}
 	return (i);
