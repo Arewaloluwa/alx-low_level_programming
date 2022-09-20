c22e1be335aa67678255ce7f3dbe5b2e8aa32a3c
Arewaloluwa <arewaakinbayo@gmail.com>
2022-09-20 16:28:50 -0700

    kji

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * strlen - returns the length of a string
+ *
+ * Return - SUCCESS
+ *
+ */
+int _strlen(char *s)
+{
+	int i;
+	
+	for (i = 0, s[i] != '\0'; i++)
+	{}
+	return (i);
+}
