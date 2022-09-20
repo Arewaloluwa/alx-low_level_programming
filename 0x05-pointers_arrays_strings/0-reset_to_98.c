c1ba5aecb29f51f92f0cf8f4e2fc7993f6a8f7bd
Arewaloluwa <arewaakinbayo@gmail.com>
2022-09-20 15:54:36 -0700

    fir

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,18 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * main - resets value to 98
- * n: pointer integer
+ * main - rest value
+ *
+ * Return: alwys 0
  */
-void reset_to_98(int *n)
+int main(void)
 {
-	*n = 98;
+	int n;
+
+	n = 402;
+	_putchar("n=%d\n", n);
+	reset_to_98(&n);
+	_putchar("n=%d\n", n);
+	return (0);
 }
-'\n'
